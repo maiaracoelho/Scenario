eefb9f0ac76409f03ffdc54ef96748818ba783a9
Berg <vandenberg.jutai@gmail.com>
2017-01-09 16:14:29 -0400

    Refatoracao de codigo

--- ManagementTC.py
+++ ManagementTC.py
@@ -7,34 +7,77 @@ import sys
 
 path_file = "/home/berg/PycharmProjects/Scripts/bw_fluctuation_claro_mao_evening_AVBW.txt"
 path_tc = '/home/berg/PycharmProjects/Scripts/tc.bash'
-
 file = open(path_file, "r")
-flag = -1
-linePrevious = file.readline().split()
+section_in_seconds = 900
 
-print("Starting simulation to test")
+def run(start = 0, interval = True):
+    scenario_lst = Scenario(file)
+    start = start * 60
+    index = 0
 
-def printf(string):
-    sys.stdout.write(string)
-    sys.stdout.flush()
+    if start != 0: index = scenario_lst.search_start(start)
 
-for line in file:
-    line = line.split()
+    for i, obj in enumerate(scenario_lst.lst[index:]):
 
-    if flag == -1:
-        flag = 0
-        subprocess.call(['sudo', '-S', path_tc, 'start', 'UP:' + linePrevious[2] + "kbit"])
-    else:
-        subprocess.call(['sudo', '-S', path_tc, 'update', 'UP:' + linePrevious[2] + "kbit"])
+        if not scenario_lst.run:
+            subprocess.call(['sudo', '-S', path_tc, 'start', 'UP:' + obj.rate + "kbit"])
+            scenario_lst.run = True
+        else:
+            subprocess.call(['sudo', '-S', path_tc, 'update', 'UP:' + obj.rate + "kbit"])
 
-    sleep_time = long(line[1]) - long(linePrevious[1])
-    linePrevious = line
-    #if linePrevious[1] >= 900: break # secao de 15 minutos
-    print("Sleep of " + str(sleep_time) + " seconds to speed of " + str(linePrevious[2]) + " UP kbit", end='\r')
-    sys.stdout.flush()
-    time.sleep(sleep_time)
+        sleep = obj.seconds - obj.seconds_prev
+        time.sleep(sleep)
+        print("Sleep of " + str(sleep) + " seconds to speed of " + obj.rate + " UP kbit", end='\r')
+        sys.stdout.flush()
 
-subprocess.call(['sudo', '-S', path_tc,'stop'])
-subprocess.call(['sudo', '-S', path_tc,'show'])
+        if interval:
+            if obj.seconds_prev >= (long(start) + section_in_seconds): break # secao de 15 minutos
 
-print("finish...")
+    subprocess.call(['sudo', '-S', path_tc, 'stop'])
+    subprocess.call(['sudo', '-S', path_tc, 'show'])
+    print("Finish...")
+
+
+class Data():
+
+    def __init__(self, hour, seconds, rate, seconds_prev):
+        self.hour = str(hour)
+        self.seconds = long(seconds)
+        self.rate = str(rate)
+        self.seconds_prev = long(seconds_prev)
+
+    def printData(self):
+        print(str(self.hour) + ' ' + str(self.seconds) + ' ' + str(self.rate))
+
+    def split(self):
+        return [self.hour, self.seconds, self.rate]
+
+class Scenario():
+    lst = []
+
+    def __init__(self, file):
+        self.read(file)
+        self.run = False
+
+    def read(self, file):
+        prev = -1
+        for line in file:
+            line = line.strip().split()
+            self.lst.append(Data(line[0], line[1], line[2], prev))
+            prev = line[1]
+
+    def search_start(self, start):
+        position = 0
+
+        while position < len(self.lst) and long(self.lst[position].split()[1]) <= start:
+            index = position
+            position +=1
+
+        return index
+
+    def printScenario(self):
+        [data.printData() for data in self.lst]
+
+
+if len(sys.argv) > 1: run(sys.argv[1], sys.argv[2])
+else: run()
