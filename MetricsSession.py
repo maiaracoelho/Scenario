2053cd904760533ab02498b2047dd100750ed1c7
Berg <vandenberg.jutai@gmail.com>
2017-01-22 01:42:28 -0400

    metrics (part 4)

--- MetricsSession.py
+++ MetricsSession.py
@@ -7,9 +7,9 @@ n_sharing = 3
 
 class MetricsSession():
 
-    def __init__(self, averageBandwidthScenario, path_json):
+    def __init__(self, averageBandwidthScenario, path_log):
         self.averageBandwidthScenario = long(averageBandwidthScenario)
-        self.path_json = str(path_json)
+        self.path_log = str(path_log)
 
     def strToJson(self, str):
         return json.loads(str)
@@ -25,8 +25,19 @@ class MetricsSession():
         r_obtained = self.averageBitrate()
         r_expected = None
 
+        sum1 = sum([r_obtained / r_expected for i in range(1, n_sharing+1)]) ** 2
+        sum2 = [(r_obtained / r_expected) ** 2 for i in range(1, n_sharing+1)]
+
+        return sum1 / sum2
+
     def instability(self):
-        pass
+        lst_chunk = self.strToJson(input_file)['chunk']
+
+        sum1 = sum([abs(lst_chunk[i]['bitrate'] - lst_chunk[i-1]['bitrate'] * i+2) for i, obj in enumerate(lst_chunk)
+                    if i-1 >= 0 and i < len(lst_chunk)-1])
+        sum2 = sum([lst_chunk[i]['bitrate'] * i for i, obj in enumerate(lst_chunk) if i > 1])
+
+        return sum1 / sum2
 
     def expectedResult(self, averageVideosRate, n_clients):
         pass
@@ -34,7 +45,8 @@ class MetricsSession():
     def averageInterruptions(self):
         lst_interruptions = self.strToJson(input_file)['interruptions']
 
-        sum1 = sum(
-            [lst_interruptions[i]['end'] - lst_interruptions[i]['start'] for i, obg in enumerate(lst_interruptions)])
+        sum1 = sum([lst_interruptions[i]['end'] - lst_interruptions[i]['start'] for i, obg in enumerate(lst_interruptions)])
         return sum1 / len(lst_interruptions)
 
+m = MetricsSession(0, "")
+print(m.instability())
