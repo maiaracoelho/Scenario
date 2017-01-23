41fb9325bd7d5f3f398c6eb22651216597fbd31c
Berg <vandenberg.jutai@gmail.com>
2017-01-23 17:47:29 -0400

    metrics (part 8)

--- Graphics.py
+++ Graphics.py
@@ -1,18 +1,9 @@
-# import matplotlib.pyplot as plt
-#
-# import pl plotly.plotly as py
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-import matplotlib.pyplot as plt
+data = [go.Bar(
+            x=['giraffes', 'orangutans', 'monkeys'],
+            y=[20, 14, 23]
+    )]
 
-# Learn about API authentication here: https://plot.ly/python/getting-started
-# Find your api_key here: https://plot.ly/settings/api
-
-y = [3, 10, 7, 5, 3, 4.5, 6, 8.1]
-N = len(y)
-x = range(N)
-width = 1/1.5
-plt.bar(x, y, width, color="blue")
-
-
-fig = plt.gcf()
-plot_url = py.plot_mpl(fig, filename='mpl-basic-bar')
+py.iplot(data, filename='basic-bar')
--- MetricsSession.py
+++ MetricsSession.py
@@ -11,6 +11,11 @@ class MetricsSession():
         self.averageBandwidthScenario = long(averageBandwidthScenario)
         self.path_log = str(path_log)
 
+
+    def averageBandwidth(self):
+        lst_bandwidth = input_file['bandwidth']
+        return sum([lst_bandwidth[i]['bandwidth'] for i, obj in enumerate(lst_bandwidth)]) / len(lst_bandwidth)
+
     def averageBitrate(self):
         j = input_file
         lst_chunk = j['chunk']
@@ -19,10 +24,10 @@ class MetricsSession():
         return sum1 / (j['session_time'] / 1000)
 
     def justice(self):
-        r_obtained =
-        r_expected = None
+        r_obtained = self.averageBitrate()
+        r_expected = r_expected()
 
-        sum1 = sum([self.averageBitrate() / r_expected for i in range(0, n_sharing)]) ** 2
+        sum1 = sum([r_obtained / r_expected for i in range(0, n_sharing)]) ** 2
         sum2 = sum([(r_obtained / r_expected) ** 2 for i in range(0, n_sharing)])
 
         return sum1 / sum2
@@ -32,20 +37,20 @@ class MetricsSession():
 
         sum1 = sum([abs(lst_chunk[i]['bitrate'] - lst_chunk[i-1]['bitrate'] * len(lst_chunk) - i) for i, obj in enumerate(lst_chunk)
                     if i-1 >= 0 and i < len(lst_chunk)-1])
-        print(sum1)
         sum2 = sum([lst_chunk[i]['bitrate'] * len(lst_chunk) - i for i, obj in enumerate(lst_chunk[1:])])
-        print(sum2)
 
         return float(sum1) / float(sum2)
 
-    def expectedResult(self, averageVideosRate, n_clients):
-        pass
+    def r_expected(self):
+        lst_qualities = input_file['qualities']
+        averageQualities = sum([lst_qualities[i]['width'] for i, obj in enumerate(lst_qualities)]) / len(lst_qualities)
+        return (averageQualities / n_sharing) * self.averageBandwidth()
 
     def averageInterruptions(self):
-        lst_interruptions = input_file['interruptions']
+        lst_interruptions = input_file['interruption']
 
         sum1 = sum([lst_interruptions[i]['end'] - lst_interruptions[i]['start'] for i, obg in enumerate(lst_interruptions)])
         return sum1 / len(lst_interruptions)
 
 m = MetricsSession(0, "")
-print(m.instability())
+print(m.r_expected())
