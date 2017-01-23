a2233a28d3d55628ac989ec4d5736657721f01dc
Berg <vandenberg.jutai@gmail.com>
2017-01-23 13:55:11 -0400

    metrics (part 8)

--- MetricsSession.py
+++ MetricsSession.py
@@ -16,25 +16,27 @@ class MetricsSession():
         lst_chunk = j['chunk']
 
         sum1 = sum([lst_chunk[i]['bitrate'] * (lst_chunk[i]['duration'] / 1000) for i, obj in enumerate(lst_chunk)])
-        return sum1 / j['session_time']
+        return sum1 / (j['session_time'] / 1000)
 
     def justice(self):
-        r_obtained = self.averageBitrate()
+        r_obtained =
         r_expected = None
 
-        sum1 = sum([r_obtained / r_expected for i in range(1, n_sharing+1)]) ** 2
-        sum2 = [(r_obtained / r_expected) ** 2 for i in range(1, n_sharing+1)]
+        sum1 = sum([self.averageBitrate() / r_expected for i in range(0, n_sharing)]) ** 2
+        sum2 = sum([(r_obtained / r_expected) ** 2 for i in range(0, n_sharing)])
 
         return sum1 / sum2
 
     def instability(self):
         lst_chunk = input_file['chunk']
 
-        sum1 = sum([abs(lst_chunk[i]['bitrate'] - lst_chunk[i-1]['bitrate'] * i+100) for i, obj in enumerate(lst_chunk)
+        sum1 = sum([abs(lst_chunk[i]['bitrate'] - lst_chunk[i-1]['bitrate'] * len(lst_chunk) - i) for i, obj in enumerate(lst_chunk)
                     if i-1 >= 0 and i < len(lst_chunk)-1])
-        sum2 = sum([lst_chunk[i]['bitrate'] * 1 for i, obj in enumerate(lst_chunk[1:])])
+        print(sum1)
+        sum2 = sum([lst_chunk[i]['bitrate'] * len(lst_chunk) - i for i, obj in enumerate(lst_chunk[1:])])
+        print(sum2)
 
-        return sum1 / sum2
+        return float(sum1) / float(sum2)
 
     def expectedResult(self, averageVideosRate, n_clients):
         pass
